99c3c5272a06182515cf95e19a204c7b234785d8
Noey Ignacio <noeyislearning@gmail.com>
2023-06-02 19:59:21 +0800

    Implement dynamic routing on the `footer`

--- components/layouts/Footer/Footer.tsx
+++ components/layouts/Footer/Footer.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import Link from "next/link";
 
+/** --- Interfaces/Props --- */
+import { FooterMenuItemProps } from "@/assets/ts/interfaces/menu"
+
+/** --- Models --- */
+import menuData from "@/assets/models/footerMenu.json"
+
 export default function Footer() {
   return (
     <>
       <footer className="px-6 md:px-12 lg:px-36 pt-16 pb-36">
         <div className="flex flex-col md:flex-row lg:flex-row gap-12 md:gap-0 lg:gap-0 items-center justify-center md:justify-between lg:justify-between">
           <div className="flex flex-row gap-5 text-sm">
-            <Link href={"https://www.instagram.com/noeyislearning/"} target={"_blank"}>
-              <div className="text-black hover:text-blue-500 underline underline-offset-4">Instagram</div>
-            </Link>
-            <Link href={"https://twitter.com/noeyislearning"} target={"_blank"}>
-              <div className="text-black hover:text-blue-500 underline underline-offset-4">Twitter</div>
-            </Link>
-            <Link href={"https://www.linkedin.com/in/noeyislearning/"} target={"_blank"}>
-              <div className="text-black hover:text-blue-500 underline underline-offset-4">LinkedIn</div>
-            </Link>
+            {menuData.menu.map((menuItem: FooterMenuItemProps) => (
+              <Link key={menuItem.name} href={menuItem.link} target={"_blank"}>
+                <div className="text-black hover:text-blue-500 underline underline-offset-4">{menuItem.name}</div>
+              </Link>
+            ))}
           </div>
           <div className="font-base text-sm text-gray-400">&copy; 2023 Noey Ignacio. All Rights Reserved.</div>
         </div>
