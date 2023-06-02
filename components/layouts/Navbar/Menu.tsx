0c1c1c400086492273d8efe11abda17b61d086be
Noey Ignacio <noeyislearning@gmail.com>
2023-06-02 19:57:49 +0800

    Refactor `Menu.tsx` for more maintainability

--- components/layouts/Navbar/Menu.tsx
+++ components/layouts/Navbar/Menu.tsx
@@ -2,46 +2,31 @@ import Link from "next/link"
 import React from 'react'
 
 /** --- Interfaces/Props --- */
-import { MenuProps } from "@/assets/ts/interfaces/menu"
+import { NavMenuProps } from "@/assets/ts/interfaces/menu"
 
 /** --- Models --- */
-import menuData  from "@/assets/models/menu.json"
+import menuData  from "@/assets/models/navMenu.json"
 
-export default function Menu({}: MenuProps) {
-
-  const menuItems = menuData.menu;
+export default function Menu({}: NavMenuProps) {
 
   return (
     <>
       <ul className="flex flex-col text-sm font-base mt-4 lg:mt-0 md:flex-row lg:space-x-8">
-        {menuItems.map((menuItem, index) => (
-          <li key={index}>
+        {menuData.menu.map((menuItem) => (
+          <li key={menuItem.name}>
             {menuItem.isAvailable ? (
-              <Link href={menuItem.isEmail ? `mailto:${menuItem.link}` : menuItem.link}>
-                <div className="block py-2 pl-3 pr-4 lg:p-0 text-black hover:underline underline-offset-4" style={{ cursor: 'pointer' }}>
-                  {menuItem.isEmail ? (
-                      <>/{menuItem.name}</>
-                  ) : (
-                      <>/{menuItem.name}</>
-                  )}
+              <Link href={menuItem.link}>
+                <div className="block py-2 pl-3 pr-4 lg:p-0 text-black hover:underline underline-offset-4 cursor-pointer">
+                  /{menuItem.name}
                 </div>
               </Link>
             ) : (
-              <div className="block py-2 pl-3 pr-4 lg:p-0 text-black" style={{ cursor: 'not-allowed' }}>
-                {menuItem.isEmail ? (
-                    <>/{menuItem.name}</>
-                ) : (
-                    <>/{menuItem.name}</>
-                )}
+              <div className="block py-2 pl-3 pr-4 lg:p-0 text-black cursor-not-allowed">
+                /{menuItem.name}
               </div>
             )}
           </li>
         ))}
-        <li>
-          <Link href={"mailto:hello@noeyislearning.dev"}>
-            <div className="block py-2 pl-3 pr-4 lg:p-0 text-black hover:underline underline-offset-4"></div>
-          </Link>
-        </li>
       </ul>
     </>
   )
