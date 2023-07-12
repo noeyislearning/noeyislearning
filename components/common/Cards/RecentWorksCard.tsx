c389259476e05680f93fe89ccfe0dbe38091ccfa
Noey Ignacio <hello@noeyislearning.dev>
2023-07-12 15:11:47 +0800

    refactor(state): Apply state management

--- components/common/Cards/RecentWorksCard.tsx
+++ components/common/Cards/RecentWorksCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,16 +8,29 @@ import {
   LinkIcon
 } from "@heroicons/react/24/outline";
 
-export default function RecentWorksCard() {
+import type { Work } from "@/redux/works/workSlice";
+
+interface Props {
+  work: Work;
+}
+
+export default function RecentWorksCard({ work }: Props) {
+
+  const trimURLPrefix = (url: string) => {
+    const urlPrefix = "https://";
+    const trimmedURL = url.slice(urlPrefix.length);
+    return trimmedURL;
+  }
+
   return (
     <figure className="max-w-screen group">
       <Link href={"/"}>
         <Image 
-          src="https://i.imgur.com/3GPl4pV.png"
+          src={work.imgURL}
           alt="Project Image"
           width={1000}
           height={1000}
-          className="h-96 max-w-screen rounded-lg object-cover mx-auto" 
+          className="h-96 mx-auto max-w-screen rounded-lg object-cover  duration-500 ease-in-out transform hover:scale-105" 
           priority
         />
       </Link>
@@ -24,25 +38,31 @@ export default function RecentWorksCard() {
         <div className="flex flex-row justify-between items-center">
           <div className="flex flex-col">
             <div className="flex flex-row gap-2 items-baseline">
-              <h3 className="text-base text-white">Zoiquery</h3>
+              <h3 className="text-base text-white">{work.name}</h3>
               <span className="text-gray-500">•</span>
-              <p className="text-sm text-gray-500">Artificial Intelligience SQL Generator</p>
+              <p className="text-sm text-gray-500">{work.description}</p>
             </div>
             <div className="flex flex-row gap-2 items-baseline">
-              <span className="text-sm text-gray-500">TypeScript</span>
-              <span className="text-sm text-gray-500">NextJS</span>
-              <span className="text-sm text-gray-500">OpenAI</span>
+              {work.technologyUsed.map((technologyUsed) => (
+                <span key={technologyUsed} className="text-sm text-gray-500">{technologyUsed}</span>
+              ))}
             </div>
           </div>
           <div className="flex flex-row gap-2 items-center">
-            <div className="text-sm text-gray-500 group-hover:text-white">
-              <Link href={"/"}>
-                <div className="flex flex-row gap-1 items-center">
-                  <LinkIcon className="w-3 h-3"/>
-                  <span className="text-xs">github.com/noeyislearning/zoiquery</span>
-                </div>
-              </Link>
-            </div>
+          {work.redirectURL ? (
+              <div className="text-sm text-gray-500 group-hover:text-white">
+                <Link href={trimURLPrefix(work.redirectURL)}>
+                  <div className="flex flex-row gap-1 items-center">
+                    <LinkIcon className="w-3 h-3"/>
+                    <span className="text-xs">{trimURLPrefix(work.redirectURL)}</span>
+                  </div>
+                </Link>
+              </div>
+            ) : (
+              <div className="text-sm text-gray-500">
+                No link available
+              </div>
+            )}
           </div>
         </div>
       </figcaption>
--- components/layouts/Works/RecentWorks.tsx
+++ components/layouts/Works/RecentWorks.tsx
@@ -1,12 +1,22 @@
 "use client";
 import React from "react";
+
+/** Redux */
+import type { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+
+/** Components */
 import RecentWorksCard from '@/components/common/Cards/RecentWorksCard';
 
 export default function RecentWorks() {
+  const works = useSelector((state: RootState) => state.work.works);
+
   return (
     <div className="flex mt-16">
       <div className="flex flex-col gap-8 items-center">
-        <RecentWorksCard />
+        {works.map((work) => (
+          <RecentWorksCard key={work.id} work={work} />
+        ))}
       </div>
     </div>
   );
