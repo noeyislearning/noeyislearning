6287cff0bed090a58bef0a7bdcfb8c42475a4518
We Publicized <hello@noeyislearning.dev>
2023-08-24 23:39:20 +0800

    refactor(preview): Change social preview

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export const siteConfig = {
   name: "Noey Ignacio",
   url: "https://noeyislearning.dev",
-  ogImage: "https://i.imgur.com/lNY7u6T.png",
-  fbOgImage: "https://i.imgur.com/lNY7u6T.png",
-  messOgImage: "https://i.imgur.com/lNY7u6T.png",
+  ogImage: "https://i.imgur.com/YRF4v1o.png",
+  fbOgImage: "https://i.imgur.com/YRF4v1o.png",
+  messOgImage: "https://i.imgur.com/YRF4v1o.png",
   description:
     "Embark on my transformative journey from software developer to data scientist. Dive into a rich repository of insights, blogs, and projects . Witness my evolution, from keen developer insights to cutting-edge data analysis. Join me as I blend software development prowess with data analytics expertise.",
 };
