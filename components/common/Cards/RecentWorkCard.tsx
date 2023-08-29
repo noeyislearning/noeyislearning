8f8c76cec01ffd7d2db9b2358d471d18e6a25e58
We Publicized <hello@noeyislearning.dev>
2023-08-29 21:48:41 +0800

    feat(data): Add new data and implement new UI

--- components/common/Cards/RecentWorkCard.tsx
+++ components/common/Cards/RecentWorkCard.tsx
@@ -23,24 +23,34 @@ export default function RecentWorkCard({ work }: Props) {
     <figure className="max-w-screen group">
       { work.redirectURL ? (
         <Link href={work.redirectURL} target={"_blank"}>
-          <Image
-            src={work.imgURL}
-            alt="Project Image"
-            width={1000}
-            height={1000}
-            className="max-w-screen mx-auto transform rounded-lg object-cover duration-500 ease-in-out hover:scale-105 lg:h-96"
-            priority
-          />
+          <div className="relative transform ease-in-out hover:scale-105 duration-500">
+            <Image
+              src={work.imgURL}
+              alt="Project Image"
+              width={1000}
+              height={1000}
+              className="max-w-screen mx-auto rounded-lg object-cover lg:h-96"
+              priority
+            />
+            <div className={`ribbon absolute -top-2 -left-2 h-40 w-40 overflow-hidden before:absolute before:top-0 before:right-0 before:-z-[1] before:border-4 ${work.category === "web" ? "before:border-slate-800 after:border-slate-800" : "before:border-rose-800 after:border-rose-800"} after:absolute after:left-0 after:bottom-0 after:-z-[1] after:border-4`}>
+              <div className={`absolute -left-14 top-[43px] w-60 -rotate-45 ${work.category === "web" ? "bg-slate-800" : "bg-rose-800"} py-2.5 text-center text-white shadow-md`}><span className="text-sm">{ work.category === "web" ? "Web Development" : "Data Science" }</span></div>
+            </div>
+          </div>
         </Link>
       ): (
-        <Image
-          src={work.imgURL}
-          alt="Project Image"
-          width={1000}
-          height={1000}
-          className="max-w-screen mx-auto transform rounded-lg object-cover duration-500 ease-in-out hover:scale-105 lg:h-96 cursor-pointer"
-          priority
-        />
+        <div className="relative transform ease-in-out hover:scale-105 duration-500">
+            <Image
+              src={work.imgURL}
+              alt="Project Image"
+              width={1000}
+              height={1000}
+              className="max-w-screen mx-auto rounded-lg object-cover lg:h-96"
+              priority
+            />
+            <div className={`ribbon absolute -top-2 -left-2 h-40 w-40 overflow-hidden before:absolute before:top-0 before:right-0 before:-z-[1] before:border-4 ${work.category === "web" ? "before:border-slate-800 after:border-slate-800" : "before:border-rose-800 after:border-rose-800"} after:absolute after:left-0 after:bottom-0 after:-z-[1] after:border-4`}>
+              <div className={`absolute -left-14 top-[43px] w-60 -rotate-45 ${work.category === "web" ? "bg-slate-800" : "bg-rose-800"} py-2.5 text-center text-white shadow-md`}><span className="text-sm">{ work.category === "web" ? "Web Development" : "Data Science" }</span></div>
+            </div>
+          </div>
       )}
       <figcaption className="mt-4">
         <div className="flex flex-col items-center justify-between gap-2 lg:flex-row">
