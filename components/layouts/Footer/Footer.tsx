0dc6c6383ae74aba0a1c73db0384c90a95485334
Noey Ignacio <hello@noeyislearning.dev>
2023-11-01 20:19:24 +0800

    refactor/ui+works: add works, repositories

--- components/layouts/Footer/Footer.tsx
+++ components/layouts/Footer/Footer.tsx
@@ -7,15 +7,15 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 flex flex-col justify-center border-t border-dashed bg-indigo-700">
+    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 flex flex-col gap-2 justify-center border-t border-dashed bg-indigo-700">
+      <ul className="w-full flex flex-row space-x-4 flex-wrap items-center justify-center text-base font-medium">
+        <li><Link href={"/"} target={"_blank"} className="hover:underline hover:underline-offset-4">Blogs</Link></li>
+        <li><Link href={"/"} target={"_blank"} className="hover:underline hover:underline-offset-4">Leetcode Solutions</Link></li>
+        <li><Link href={"/"} target={"_blank"} className="hover:underline hover:underline-offset-4">About</Link></li>
+        <li><Link href={"https://noeylearning.gumroad.com"} target={"_blank"} className="hover:underline hover:underline-offset-4">Shop</Link></li>
+      </ul>
       <span className="text-base text-center">© {currentYear} <Link href="https://noeyislearning.dev" className="text-yellow-400 hover:underline hover:underline-offset-4">Noeyislearning™</Link>. All Rights Reserved.
       </span>
-      <ul className="w-full flex flex-row space-x-4 flex-wrap items-center justify-center text-base font-medium">
-        <li><Link href={""} target={"_blank"} className="hover:underline hover:underline-offset-4">Blogs</Link></li>
-        <li><Link href={""} target={"_blank"} className="hover:underline hover:underline-offset-4">Leetcode</Link></li>
-        <li><Link href={""} target={"_blank"} className="hover:underline hover:underline-offset-4">About</Link></li>
-        <li><Link href={""} target={"_blank"} className="hover:underline hover:underline-offset-4">Free E-books</Link></li>
-      </ul>
     </footer>
   )
 }
--- components/layouts/Works/Works.tsx
+++ components/layouts/Works/Works.tsx
@@ -1,15 +1,25 @@
+"use client";
 import React from "react";
 
+/** Components */
 import WorkCard from "@/components/common/Cards/WorkCard";
 
+/** Redux related */
+import type { Work } from "@/redux/works/reducer";
+import type { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+
 export default function Works() {
+
+  // Get works from redux store
+  const works = useSelector<RootState, Work[]>(state => state.work.works);
+
   return (
     <div className="pt-8 pb-24 flex flex-row w-full max-w-5xl justify-between items-center">
-      <div className="grid grid-cols-1 gap-4 w-full md:grid-cols-2 lg:grid-cols-3">
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
+      <div className="grid grid-cols-1 gap-6 w-full h-full md:grid-cols-2 lg:grid-cols-3">
+        {works.map((work, index) => (
+          <WorkCard key={index} work={work} />
+        ))}
       </div>
     </div>
   )
