28afab8d6927219ac6ef7623f333fd6a414f5411
Noey Ignacio <hello@noeyislearning.dev>
2023-11-02 14:36:59 +0800

    refactor/logo: separate logo from navbar

--- components/layouts/Navbar/Navbar.tsx
+++ components/layouts/Navbar/Navbar.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Link from "next/link";
 
+import Logo from "@/components/common/Logos/Logo";
+
 export default function Navbar() {
   return (
     <nav className="pt-16 mx-auto w-full flex flex-col items-center">
       <div className="flex flex-col md:flex-col gap-6 lg:flex-row w-full max-w-5xl justify-center md:justify-center lg:justify-between items-center">
-        <Link href={"/"}>
-          <div className="flex flex-col text-center md:text-center lg:text-start">
-            <h1 className="text-4xl">Noeyislearning</h1>
-            <span className="-mt-1 text-sm">/Francis Ignacio/</span>
-          </div>
-        </Link>
+        <Logo />
         <div className="flex flex-row gap-1 md:gap-4 lg:gap-4 items-center md:flex lg:flex ">
           <Link href={"https://www.linkedin.com/in/noeyislearning/"} target={"_blank"} className="py-1 px-6 border border-dashed rounded-full hover:text-black hover:border-yellow-500 hover:bg-yellow-500">
             <span>LinkedIn</span>
