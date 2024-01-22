329969524976c380828c175fd6c11e015e1d37ce
Noey Ignacio <hello@noeyislearning.dev>
2024-01-22 16:38:15 +0800

    fix(algo): add 'path.resolve' in 'post.ts' logic

--- src/libs/helper/post.ts
+++ src/libs/helper/post.ts
@@ -4,15 +4,18 @@ import matter from "gray-matter";
 /** Interfaces */
 import { MetadataProps } from "@/libs/interfaces";
 
-export const getPostMetadata = (folder: string): MetadataProps[] => {
-  const files = fs.readdirSync(folder);
-  
-  const posts = files.map((fn) => {
-    const filePath = path.join(folder, fn);
-    const fileContents = fs.readFileSync(filePath, "utf8");
-    const matterResult = matter(fileContents);
+const readAndParseFile = (filePath: string) => {
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  return matter(fileContents);
+};
 
-    const { name, description, is_active, thumbnail_URL, dev_date, content } = matterResult.data;
+export const getPostMetadata = (folder: string): MetadataProps[] => {
+  const absolutePath = path.resolve(process.cwd(), folder);
+  const files = fs.readdirSync(absolutePath);
+  
+  return files.map((fn) => {
+    const filePath = path.join(absolutePath, fn);
+    const { data: { name, description, is_active, thumbnail_URL, dev_date, content } } = readAndParseFile(filePath);
 
     return {
       slug: fn.replace(".md", ""),
@@ -24,16 +27,11 @@ export const getPostMetadata = (folder: string): MetadataProps[] => {
       content
     };
   });
-
-  return posts;
 };
 
 export const readMetadataFromMdFile = (filePath: string): MetadataProps => {
-  const fileContents = fs.readFileSync(filePath, "utf8");
-  const matterResult = matter(fileContents);
-
-  const { name, description, is_active, thumbnail_URL, dev_date } = matterResult.data;
-  const content = matterResult.content;
+  const absolutePath = path.resolve(process.cwd(), filePath);
+  const { data: { name, description, is_active, thumbnail_URL, dev_date }, content } = readAndParseFile(absolutePath);
 
   return {
     slug: path.basename(filePath, '.md'),
