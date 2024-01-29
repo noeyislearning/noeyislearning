6d62641550f755cb66c9730f11184f6462e80f27
Ibrahim Lincoln <noeyislearning@gmail.com>
2024-01-29 22:43:48 +0800

    style(ui): update placeholder style in page components

--- src/app/(pages)/me/page.tsx
+++ src/app/(pages)/me/page.tsx
@@ -10,7 +10,9 @@ export default function MePage() {
       <TranslateYOpacity>
         <div className="flex h-full w-full flex-col items-center justify-center">
           <h1 className="text-3xl text-zinc-300">Who am I?</h1>
-          <p className="text-zinc-300">Will be posting bout myself. For now it&apos;s empty.</p>
+          <p className="max-w-xs text-center text-sm text-zinc-500">
+            Will be posting bout myself. For now it&apos;s empty.
+          </p>
         </div>
       </TranslateYOpacity>
     </div>
