d4a5438f809062171ca4704b21c4ea11afb902d6
Noey Ignacio <hello@noeyislearning.dev>
2024-02-23 20:22:31 +0800

    feat/bg: add new background components

---
+++ src/components/common/backgrounds/BoxesPatternBackground.tsx
@@ -0,0 +1,5 @@
+export default function BoxedPatternBackground() {
+  return (
+    <div className="pattern-boxes absolute inset-0 z-0 h-full w-full pattern-zinc-100 pattern-opacity-20 pattern-size-6"></div>
+  )
+}
---
+++ src/components/common/backgrounds/IsometricPatternBackground.tsx
@@ -0,0 +1,5 @@
+export default function IsometricPatternBackground() {
+  return (
+    <div className="pattern-isometric absolute inset-0 z-0 h-full w-full pattern-indigo-100 pattern-opacity-10 pattern-size-32"></div>
+  )
+}
