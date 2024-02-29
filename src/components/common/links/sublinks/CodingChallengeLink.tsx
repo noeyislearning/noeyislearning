4a16cfaae49b57eb65116f24c077a98fdf984af9
Noey Ignacio <hello@noeyislearning.dev>
2024-02-29 10:01:29 +0800

    style:ui: update styling in CodingChallengeLink component

--- src/components/common/links/sublinks/CodingChallengeLink.tsx
+++ src/components/common/links/sublinks/CodingChallengeLink.tsx
@@ -13,7 +13,7 @@ export default function ProjectLink({
           ${
             fullPathname === `${trimmedPathname}/${metadata.slug}`
               ? "border-indigo-600 bg-indigo-700 text-indigo-100"
-              : "border-indigo-200 bg-indigo-50 text-indigo-600 hover:border-indigo-600 hover:bg-indigo-700 hover:text-indigo-100"
+              : "border-indigo-200 bg-zinc-100 text-indigo-600 hover:border-indigo-600 hover:bg-indigo-700 hover:text-indigo-100"
           }
         `}
       >
@@ -26,9 +26,12 @@ export default function ProjectLink({
                 : "text-zinc-400 group-hover:text-indigo-200"
             }`}
           >
-            {metadata.languages_used ? (
-              <p>[ {metadata.languages_used.join(", ")} ]</p>
-            ) : null}
+            <div className="flex items-center gap-1">
+              {metadata.languages_used ? (
+                <p>[ {metadata.languages_used.join(", ")} ]</p>
+              ) : null}
+              <p>[ {metadata.difficulty} ]</p>
+            </div>
             <p>[ {metadata.platform} ] </p>
           </div>
         </div>
