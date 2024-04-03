adba5876f2271b1e026dd48ae2149c3be4aa5720
Noey Ignacio <hello@noeyislearning.dev>
2024-04-04 07:51:55 +0800

    cleanup/:* delete unused files and configurations

--- src/components/common/dividers/SideNavSectionDivider.tsx
+++
@@ -1,8 +0,0 @@
-export default function SideNavSectionDivider({ title }: { title: string }) {
-  return (
-    <>
-      <div className="border"></div>
-      <div className="text-sm text-zinc-600">{title}</div>
-    </>
-  )
-}
--- src/types/Metadata.ts
+++
@@ -1,3 +0,0 @@
-export interface MetadataState {
-  metadata: Record<string, any>
-}
